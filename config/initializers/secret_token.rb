08bd08e0a6d0ee0d46925b74e90f9b26f395b265
Darren Smith <dsm1th@Darrens-MacBook-Pro.local>
2012-11-21 23:36:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cdx::Application.config.secret_token = '1d044834a9fe0c004bd010da7ab15bfe9cb9528134bd783117ce872dbce8d2d730e41866ae270bbadac9b89f46b8f6e7f4cecd997f9c596acfc5f12361e01e0d'
